9fcb115a9f8f0fce8db9cc4bc791b17fd8d2d7de
zhedong <zdzheng12@gmail.com>
2019-01-12 21:44:41 +1100

    add random

---
+++ select.py
@@ -0,0 +1,7 @@
+import copy, random, numpy as np
+
+people = ['ZD-Zheng', 'QJ-Guan', 'YW-Luo', 'Z-Zhun', 'YF-Sun', 'Vana', 'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li', 'XY-Dong', 'JX-Miao', 'YH-Ding', 'FD-Zhu', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'P-Liu', 'BW-Hu', 'AM-Wu']
+
+random.shuffle(people)
+
+print(people)
