15e7e66d83e2fa3357d747ce8f927dc5c49ddcde
zhedong <zdzheng12@gmail.com>
2019-04-13 13:18:15 +1000

    new order

--- select.py
+++ select.py
@@ -1,6 +1,6 @@
 import copy, random, numpy as np
 
-people = ['ZD-Zheng', 'QJ-Guan', 'YW-Luo', 'Z-Zhun', 'YF-Sun', 'Vana', 'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li', 'XY-Dong', 'JX-Miao', 'YH-Ding', 'FD-Zhu', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'P-Liu', 'BW-Hu', 'AM-Wu']
+people = ['ZD-Zheng', 'QJ-Guan', 'YW-Luo', 'Z-Zhun', 'YF-Sun', 'Vana', 'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li', 'XY-Dong', 'JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'P-Liu', 'BW-Hu', 'AM-Wu', 'T-Ruan', 'YX=Wang']
 
 random.shuffle(people)
 
