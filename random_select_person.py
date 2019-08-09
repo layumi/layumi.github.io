a68a07e3e3fc6ebdb25cf90f945902f2dc6a2bb4
zhedong <zdzheng12@gmail.com>
2019-08-09 10:49:20 +1000

    add slides

--- random_select_person.py
+++ random_select_person.py
@@ -1,6 +1,6 @@
 import copy, random, numpy as np
 
-people = ['ZD-Zheng', 'QJ-Guan', 'YW-Luo', 'Z-Zhun', 'YF-Sun', 'Vana', 'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li', 'XY-Dong', 'JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'P-Liu', 'BW-Hu', 'AM-Wu', 'T-Ruan', 'YX=Wang']
+people = ['ZD-Zheng', 'YW-Luo', 'Z-Zhun', 'YF-Sun',  'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li', 'XY-Dong', 'JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'P-Liu', 'BW-Hu', 'AM-Wu', 'Q-Rao', 'TTQ', 'YX=Wang', 'TY-Wang', 'HF-Wen', 'KP-Li', 'Hao-Wang']
 
 random.shuffle(people)
 
