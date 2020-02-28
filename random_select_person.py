c3f719ec5d3afc2c8a575921c04e47f4594843a9
zhedong <zdzheng12@gmail.com>
2020-02-29 09:29:36 +1100

    add dataset

--- random_select_person.py
+++ random_select_person.py
@@ -1,6 +1,6 @@
 import copy, random, numpy as np
 
-people = ['ZD-Zheng', 'YW-Luo', 'Z-Zhun', 'YF-Sun',  'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li', 'XY-Dong', 'JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'P-Liu', 'BW-Hu', 'AM-Wu', 'Q-Rao', 'TTQ', 'YX=Wang', 'TY-Wang', 'HF-Wen', 'KP-Li', 'Hao-Wang']
+people = ['ZD-Zheng',  'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li','JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'BW-Hu', 'AM-Wu', 'Q-Rao', 'TTQ', 'YX=Wang', 'Hu-Zhang', 'TY-Wang','KP-Li', 'Hao-Wang', 'weibo-Gong', 'Chuchu-Han', 'Xuanmeng-Zhang','Chao-Liang', 'chen-Liang']
 
 random.shuffle(people)
 
