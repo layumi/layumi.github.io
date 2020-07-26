bd176ba401bc3e6d20d2d20c250f6a506f52b217
zhedong <zdzheng12@gmail.com>
2020-07-26 13:46:40 +1000

    acm mm

--- random_select_person.py
+++ random_select_person.py
@@ -1,6 +1,6 @@
 import copy, random, numpy as np
 
-people = ['ZD-Zheng',  'WJ-Deng', 'XL-Zhang', 'Guang-Li', 'GuangRui-Li','JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'QY-Feng', 'BW-Hu', 'AM-Wu', 'Q-Rao', 'TTQ', 'YX=Wang', 'Hu-Zhang', 'TY-Wang','KP-Li', 'Hao-Wang', 'weibo-Gong', 'Chuchu-Han', 'Xuanmeng-Zhang','Chao-Liang', 'chen-Liang']
+people = ['ZD-Zheng',  'WJ-Deng', 'XiaoLin-Zhang', 'Guang-Li', 'GuangRui-Li','JX-Miao', 'YH-Ding', 'F-Ma', 'Ruijie-Quan', 'BW-Hu', 'AM-Wu', 'Q-Rao', 'TTQ', 'YX=Wang', 'Hu-Zhang', 'TY-Wang','KP-Li', 'weibo-Gong', 'Chuchu-Han', 'Xuanmeng-Zhang', 'Chao-Liang', 'Chen-Liang', 'Zheqin Lin', 'Wei Li']
 
 random.shuffle(people)
 
